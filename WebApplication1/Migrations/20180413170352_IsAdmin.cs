8b8af47274fea2d3037f3b5e770b66c287b0c1b3
Adam Dziomdziora <adam.dziomdziora@outlook.com>
2018-04-13 19:06:42 +0200

    is admin

---
+++ WebApplication1/Migrations/20180413170352_IsAdmin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class IsAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "UsersNotSercure",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "UsersNotSercure");
+        }
+    }
+}
--- WebApplication1/Models/UsersNotSercure.cs
+++ WebApplication1/Models/UsersNotSercure.cs
@@ -8,6 +8,8 @@ namespace WebApplication1.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
+
+        public bool IsAdmin { get; set; }
     }
 
     public class NamePassword
